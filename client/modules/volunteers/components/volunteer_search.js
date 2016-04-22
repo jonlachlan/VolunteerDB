1571f38b91231250f22248825d6736435da12524
Jon Lachlan <jonlachlan@gmail.com>
2016-04-22 10:14:03 -0700

    Update search component to call actions.
    Also add bootstrap styles/components using ‘react-bootstrap’ npm
    library.

--- client/modules/volunteers/components/volunteer_search.js
+++ client/modules/volunteers/components/volunteer_search.js
@@ -1,21 +1,49 @@
 import React from 'react'
 import ResultItem from './search_result_item'
+import { Panel, FormControl, Table } from 'react-bootstrap'
 
-const VolunteerSearch = ({searchString, searchResults}) => (
+const VolunteerSearch = ({
+  searchString,
+  searchResults,
+  resultsCount,
+  getResults
+}) => (
   <div>
-    <h1>Volunteer Search</h1>
-    <div className="ui input">{searchString}</div>
-    <div className="ui divided items">
-      {searchResults.map((item, index) => {
-        return (
-          <ResultItem item={item} />
-        )
-      })}
-    </div>
+    <Panel header="Volunteer Search" bsStyle="primary">
+      <p><em>For example, try "writing letters to the editor" or "web developer"</em></p>
+      <form
+        onSubmit={(e)=> {
+        e.preventDefault();
+      }}
+      >
+        <FormControl type="text" placeholder="Enter text"
+                     onChange={(e)=> {
+          e.preventDefault();
+          getResults(e.target.value);
+        }}
+        />
+      </form>
+      <h4>{resultsCount} results found.</h4>
+      <p><strong>Email / Interest</strong></p>
+      <Table responsive striped bordered condensed hover>
+        <thead>
+        <tr>
+          <th>Email</th>
+          <th>Interests</th>
+        </tr>
+        </thead>
+        <tbody>
+        {searchResults.map((result) => (
+          <tr>
+            {/** Example Only **/}
+            <td>{result.email}</td>
+            <td>{result.interests}</td>
+          </tr>
+        ))}
+        </tbody>
+      </Table>
+    </Panel>
   </div>
 );
-
-
-
 
 export default VolunteerSearch;
