643f3d7907982b9a0cb2e99d98642dd49d411073
Jon Lachlan <jonlachlan@gmail.com>
2016-04-24 23:48:16 -0700

    Fix variable reference in ES method

--- server/methods/volunteer-search.js
+++ server/methods/volunteer-search.js
@@ -13,30 +13,31 @@ import {check} from 'meteor/check';
 Meteor.methods({
 
   'volunteers.profiles.search'(searchText) {
-    check(searchText, String);
-    console.log("[getVolunteerProfiles]", searchText);
-    /** Example Only **/
-    var query = {
-      "match" : {
-        "interests": {
-          "query" : searchText,
-          "operator": "and",
-          "fuzziness" : 2
-        }
-      }
-    };
 
-    var result = ES.EsClient.search({
-      index: "easysearch",
-      type: "volunteer-profiles",
-      body: {
-        query: query
-      }
-    });
-    console.log(result);
-    return result.hits.hits.map(function(doc) {
-      return doc._source;
-    });
+    check(searchText, String);
+      console.log("[getVolunteerProfiles]", searchText);
+      /** Example Only **/
+      var query = {
+        "match" : {
+          "interests": {
+            "query" : searchText,
+            "operator": "and",
+            "fuzziness" : 2
+          }
+        }
+      };
+
+      var result = ESClient.EsClient.search({
+        index: "easysearch",
+        type: "volunteer-profiles",
+        body: {
+          query: query
+        }
+      });
+      console.log(result);
+      return result.hits.hits.map(function(doc) {
+        return doc._source;
+      });
   }
 
 });
