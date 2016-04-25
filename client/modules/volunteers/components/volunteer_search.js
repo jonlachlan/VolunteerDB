6e72b22f2b15b3ef650b24c8a206f4c2c214e2cd
Jon Lachlan <jonlachlan@gmail.com>
2016-04-24 18:18:59 -0700

    Update volunteer search styles

--- client/modules/volunteers/components/volunteer_search.js
+++ client/modules/volunteers/components/volunteer_search.js
@@ -1,49 +1,41 @@
 import React from 'react'
-import ResultItem from './search_result_item'
 import { Panel, FormControl, Table } from 'react-bootstrap'
 
-const VolunteerSearch = ({
+export default VolunteerSearch = ({
   searchString,
   searchResults,
   resultsCount,
   getResults
 }) => (
   <div>
-    <Panel header="Volunteer Search" bsStyle="primary">
-      <p><em>For example, try "writing letters to the editor" or "web developer"</em></p>
-      <form
-        onSubmit={(e)=> {
+    <h1>"Volunteer Search"</h1>
+    <p><em>For example, try "writing letters to the editor" or "web developer"</em></p>
+    <form onSubmit={(e)=> {
+      e.preventDefault();
+    }}>
+      <FormControl type="text" placeholder="Enter text" onChange={(e)=> {
         e.preventDefault();
-      }}
-      >
-        <FormControl type="text" placeholder="Enter text"
-                     onChange={(e)=> {
-          e.preventDefault();
-          getResults(e.target.value);
-        }}
-        />
-      </form>
-      <h4>{resultsCount} results found.</h4>
-      <p><strong>Email / Interest</strong></p>
-      <Table responsive striped bordered condensed hover>
-        <thead>
+        getResults(e.target.value);
+      }}/>
+    </form>
+    <h4>{resultsCount} results found.</h4>
+    <p><strong>Email / Interest</strong></p>
+    <Table responsive striped bordered condensed hover>
+      <thead>
+      <tr>
+        <th>Email</th>
+        <th>Interests</th>
+      </tr>
+      </thead>
+      <tbody>
+      {searchResults.map((result) => (
         <tr>
-          <th>Email</th>
-          <th>Interests</th>
+          {/** Example Only **/}
+          <td>{result.email}</td>
+          <td>{result.interests}</td>
         </tr>
-        </thead>
-        <tbody>
-        {searchResults.map((result) => (
-          <tr>
-            {/** Example Only **/}
-            <td>{result.email}</td>
-            <td>{result.interests}</td>
-          </tr>
-        ))}
-        </tbody>
-      </Table>
-    </Panel>
+      ))}
+      </tbody>
+    </Table>
   </div>
 );
-
-export default VolunteerSearch;
