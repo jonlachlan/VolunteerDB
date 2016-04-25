12d07e21dd52db5acb301ee12d1f0ae2296d8a2b
Jon Lachlan <jonlachlan@gmail.com>
2016-04-24 18:20:08 -0700

    Add elasticsearch configs to server main loader

--- server/main.js
+++ server/main.js
@@ -1,7 +1,10 @@
 import publications from './publications';
 import methods from './methods';
 import configs from './configs';
+import elasticsearch from './elasticsearch';
+
 
 publications();
 methods();
 configs();
+elasticsearch();
