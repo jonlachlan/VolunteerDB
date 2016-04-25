dc60a62c39139e38cbdcc9ebadc72af54b9a3df4
Jon Lachlan <jonlachlan@gmail.com>
2016-04-24 18:16:11 -0700

    Add Accounts config file and /login route
    Also, clean up core routes file from seed.

--- client/modules/core/routes.js
+++ client/modules/core/routes.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import {mount} from 'react-mounter';
-import MainLayout from '/client/modules/core/components/main_layout';
+import MainLayout from './components/main_layout';
 import Homepage from './components/homepage'
-
-//import {
-  //AuthCheck,
-  //LayoutDefault,
-  //Simplest,
-//} from '/client/configs/components.js';
 
 export default function (injectDeps, {FlowRouter}) {
 
   const MainLayoutCtx = injectDeps(MainLayout);
-
-  //const AuthCheckCtx = injectDeps(AuthCheck);
 
   FlowRouter.route('/', {
     name: 'root',
     action() {
       mount(MainLayoutCtx, {
         content: () => (<Homepage />)
+      });
+    }
+  });
+
+  FlowRouter.route('/login', {
+    name: 'login',
+    action() {
+      mount(MainLayoutCtx, {
+        content: () => (<Accounts.ui.LoginForm />)
       });
     }
   });
