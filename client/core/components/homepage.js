c9df24a2fe365345190885fa9c9595937ebe2b1b
Jon Lachlan <jonlachlan@gmail.com>
2016-05-09 16:44:41 -0700

    Use GRS logo on homepage

--- client/core/components/homepage.js
+++ client/core/components/homepage.js
@@ -7,14 +7,14 @@ export default Homepage = ({}) => (
   <div className="ui middle aligned centered two column stackable grid">
     <div className="column">
       <h1 className="ui center aligned fluid text container" style={headerStyle}>
-        <em>We</em> are the revolution.
+        Together, we can elect a Congress that represents all of us.
       </h1>
       <div className="ui large fluid primary button" onClick={()=>FlowRouter.go('volunteers.register')}>
         Let's Get Started
       </div>
     </div>
     <div className="column">
-      <img className="ui large centered image" src="resources/sanders-splash-img.jpg" />
+      <img className="ui large centered image" src="resources/grassrootsselect-logo.png" />
     </div>
   </div>
 )
