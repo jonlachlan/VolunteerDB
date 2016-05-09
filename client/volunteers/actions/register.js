19a5f8b1bec4c6e7b60f0207644456974039b59c
Jon Lachlan <jonlachlan@gmail.com>
2016-05-09 16:45:30 -0700

    Refactor VolunteerRegistration to Onboarding

--- client/volunteers/actions/register.js
+++ client/volunteers/actions/register.js
@@ -1,9 +1,11 @@
 import { LocalState } from '/client/core/context';
 import { Meteor } from 'meteor/meteor';
+import { FlowRouter } from 'meteor/kadira:flow-router'
 
 
 export function submitRegistration(data) {
   console.log(data)
+  FlowRouter.go('volunteers.dashboard')
   // todo
   //Meteor.call('createVolunteerUser', doc)
 }
