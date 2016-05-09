e21b950b3dc9c25660d20ad7dbea61db6f3c08ac
Jon Lachlan <jonlachlan@gmail.com>
2016-05-09 15:45:08 -0700

    Remove function wrapper for publication.
    This pattern is from Mantra, which has been removed from the project.

--- server/publications/UserCurrent.js
+++ server/publications/UserCurrent.js
@@ -1,8 +1,6 @@
 import {Meteor} from 'meteor/meteor';
 
-export default function () {
 
-  Meteor.publish('users.current', function () {
-    return Meteor.users.find(this.userId);
-  });
-}
+Meteor.publish('users.current', function () {
+  return Meteor.users.find(this.userId);
+});
